9692f0531efbad31019e4917fa76584058ed1d8f
andrew <kondratev-28@mail.ru>
2023-01-01 21:33:37 +0700

    current_user_can_edit hotfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     )
   end
 
-  def current_user_can_edit?(event)
+  def current_user_can_edit?(model)
     user_signed_in? && (
       model.user == current_user ||
       (model.try(:event).present? && model.event.user == current_user)
