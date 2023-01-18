e3f4982c6f9e14211f04fd401f2bad6b39d7c59a
andrew <kondratev-28@mail.ru>
2023-01-18 20:06:00 +0700

    no more secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,37 +1,39 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock '~> 3.17.1'
 
-set :application, "bbq"
-set :repo_url, "git@github.com:eternalyoung/bbq.git"
+set :application, 'bbq'
+set :repo_url, 'git@github.com:eternalyoung/bbq.git'
 set :branch, :main
 
-set :deploy_to, "/home/deploy/apps/bbq"
+set :deploy_to, '/home/deploy/apps/bbq'
 
-append :linked_files, "config/database.yml", 'config/secrets.yml'
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+append :linked_files, 'config/database.yml', 'master.key'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'public/system', 'vendor', 'storage'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# 
+# set :deploy_to, '/var/www/my_app_name'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/master.key'
 
 # Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'public/system', 'vendor', 'storage'
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :default_env, { path: '/opt/ruby/bin:$PATH' }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'bbq.kadproject.ru', user: 'deploy', roles: %w{app db web}
+server 'bbq.kadproject.ru', user: 'deploy', roles: %w[app db web]
 
 # role-based syntax
 # ==================
