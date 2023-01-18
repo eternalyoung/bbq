ad394db639b91df23ea7f3657d51cbfc85eb75b8
andrew <kondratev-28@mail.ru>
2023-01-18 21:06:32 +0700

    no more dotenv

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['MAILJET_SENDER']
+  default from: Rails.application.credentials.mailjet[:sender]
   layout 'mailer'
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV['MAILJET_SENDER']
+  config.mailer_sender = Rails.application.credentials.mailjet[:sender]
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
