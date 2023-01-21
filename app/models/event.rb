82ce60b62e43250bc1b2cd27edbb521e720a2bc3
andrew <kondratev-28@mail.ru>
2023-01-21 17:07:36 +0700

    has_many through dependent fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
-  has_many :subscribers, through: :subscriptions, source: :user, dependent: :destroy
+  has_many :subscribers, through: :subscriptions, source: :user
   has_many :photos, dependent: :destroy
 
   validates :user, presence: true
